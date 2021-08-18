9352d282d7133682f9a34bd97e3b6dadc0edbf46
Bsfla <baebeomsu77@gmail.com>
2021-08-18 15:51:35 +0900

    commit

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bs44108:bs418905@cluster0.cemfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://bs44108:bs418905@cluster0.cemfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+const { User } = require("./models/User");
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+       if(err) return res.json({ success: false, err })
+       return res.status(200).json({
+           success: true
+       })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
